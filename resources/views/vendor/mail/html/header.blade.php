fd2589ef6f667dbb6f7ba997206c2ce057d806f0
Exhore <nelsonrivca@gmail.com>
2024-03-11 00:00:36 +0000

    logo added

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            {{-- sacar el logo de lesser --}}
-            <img src="" class="logo" alt="Lesser Logo">
+            <img src="{{ asset('images/logo.svg') }}" class="logo" alt="Lesser-Logo">
         </a>
     </td>
 </tr>
