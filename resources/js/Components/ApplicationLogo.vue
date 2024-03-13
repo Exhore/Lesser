b069d24e22c90a4481dd8f278845248c5f88ab28
Exhore <nelsonrivca@gmail.com>
2024-03-13 20:05:28 +0000

    verify email + custom colors + fixed Link comp

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,5 +1,15 @@
+<script>
+import { Link } from "@inertiajs/vue3";
+
+export default {
+    components: {
+        Link
+    },
+};
+</script>
+
 <template>
     <Link :href="'/'">
-        <img src="/assets/logo.svg" alt="Logo" class="w-64 h-auto -mt-40" />
+    <img src="/assets/logo.svg" alt="Logo" class="w-64 h-auto -mt-40" />
     </Link>
 </template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -7,6 +7,7 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import Footer from '@/Components/Footer.vue';
 
 const form = useForm({
     name: '',
@@ -99,21 +100,9 @@ const submit = () => {
                 <InputError class="mt-2" :message="form.errors.phone" />
             </div>
 
-            <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
-                <InputLabel for="terms">
-                    <div class="flex items-center">
-                        <Checkbox id="terms" v-model:checked="form.terms" name="terms" required />
-
-                        <div class="ms-2">
-                            I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Privacy Policy</a>
-                        </div>
-                    </div>
-                    <InputError class="mt-2" :message="form.errors.terms" />
-                </InputLabel>
-            </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                     Already registered?
                 </Link>
 
@@ -123,4 +112,5 @@ const submit = () => {
             </div>
         </form>
     </AuthenticationCard>
+    <Footer/>
 </template>
